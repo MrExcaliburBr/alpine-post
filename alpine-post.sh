88910fe4e2a14f8b37899c3dce7ad23dc60919d6
MrExcaliburBr <joseeduardo3224@gmail.com>
2020-08-03 13:54:17 -0300

    nvim vim-clap

--- alpine-post.sh
+++ alpine-post.sh
@@ -103,6 +103,7 @@
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
     nvim -es -u init.vim -i NONE -c "PlugInstall" -c "qa"
+    nvim -es -u init.vim -i NONE -c "call clap#installer#build_maple()" -c "qa"
     
     #Suckless software 
     ##Dependencies
@@ -157,4 +158,5 @@
     #TODO Clean home directory 
     doas apk upgrade --update-cache --available
     rm -rf voidrice
+    rm install.sh
     }
